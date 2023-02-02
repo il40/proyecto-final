7046df71577beecd275ab409adf8884881ea0bc9
ismael <ismaellopez4013@gmail.com>
2023-02-02 10:44:00 -0300

    se modifico el menu con js

--- javascript.js/javascript.js
+++ javascript.js/javascript.js
@@ -3,6 +3,8 @@ const navegadorMenu = document.getElementById('navegador-menu');
 const abrirMenu = document.getElementById('abrir-menu');
 const cerrarMenu = document.getElementById('cerrar-menu');
 
+
+
 abrirMenu.addEventListener('click', () =>{
     navegadorMenu.classList.toggle('verMenu');
 })
@@ -10,6 +12,29 @@ abrirMenu.addEventListener('click', () =>{
 cerrarMenu.addEventListener('click', () =>{
     navegadorMenu.classList.remove('verMenu');
 })
+
+const navItem1 = document.getElementById('nav-item1');
+const navItem2 = document.getElementById('nav-item2');
+const navItem3 = document.getElementById('nav-item3');
+const navItem4 = document.getElementById('nav-item4');
+const navItem5 = document.getElementById('nav-item5');
+const navItem6 = document.getElementById('nav-item6');
+const navItem7 = document.getElementById('nav-item7');
+
+navItem1.addEventListener('click', close);
+navItem2.addEventListener('click', close);
+navItem3.addEventListener('click', close);
+navItem4.addEventListener('click', close);
+navItem5.addEventListener('click', close);
+navItem6.addEventListener('click', close);
+navItem7.addEventListener('click', close);
+
+function close(){
+    navegadorMenu.classList.remove('verMenu');
+}
+
+
+
 
 
 
